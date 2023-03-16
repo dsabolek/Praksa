2995cfdb2dd24fa95330f426a7f4fa289c25cb38
struk3l <127092711+struk3l@users.noreply.github.com>
2023-03-16 08:44:42 +0100

    15.3

--- app/public/wp-admin/Export_narudzbe.php
+++ app/public/wp-admin/Export_narudzbe.php
@@ -15,13 +15,22 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+    
+$podaci1 = $wpdb->get_results("SELECT order_id, date_created, num_items_sold, net_total, status, customer_id FROM `wp_wc_order_stats` ");
+     foreach ($podaci1 as $podaci1);
 
-$header_args = array( ‘id narudžbe’, ‘datum narudžbe’, ‘podaci o kupcu’, ‘podaci o dostavi’, 
-                    ‘podaci o plaćanju’, ‘status’, ‘podaci o proizvodima’);
+$podaci2 = $wpdb->get_results("SELECT order_item_id, order_item_name FROM `wp_woocommerce_order_items` ");
+    foreach ($podaci2 as $podaci2);
+ $rezultat = $podaci1 . ' ' . $podaci2;
+      echo $rezultat ->post_title;
+
+$header_args = array( ‘ id narudžbe’, ‘datum narudžbe’, ‘podaci o kupcu’, ‘podaci o dostavi’, 
+                    ‘o plaćanju’, ‘status’, ‘podaci o proizvodima’);
     header('Content-Type: text/csv; charset=utf-8');
     header('Content-Disposition: attachment; filename=csv_export.csv');
     $output = fopen( 'php://output', 'w' );
     ob_end_clean();
     fputcsv($output, $header_args);
     exit();
+
     ?>
