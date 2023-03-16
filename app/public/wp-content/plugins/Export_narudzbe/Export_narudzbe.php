bdcfd030dce71c21cb7575cbfbf2370dd0199a6f
struk3l <127092711+struk3l@users.noreply.github.com>
2023-03-16 10:13:53 +0100

    16.3

---
+++ app/public/wp-content/plugins/Export_narudzbe/Export_narudzbe.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Plugin Name: Export narudžbe
+ * Description: Plugin ispisuje narudžbe u csv formatu.
+ */
+// connect to database
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "local";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$p1 = "SELECT `order_item_id`, `order_id`, `product_id`, `date_created`, `product_gross_revenue` FROM `wp_wc_order_product_lookup`";
+$p2 = "SELECT `order_item_name`, `order_item_id` FROM `wp_woocommerce_order_items`";
+$p3 = "SELECT `status`, `customer_id`, `order_id` FROM `wp_wc_order_stats` ";
+$ispis = $p1 . ' ' . $p2 . ' ' . $p3;
+$result = $conn->query($ispis);
+
+$n_dok = "exportNarudzi.csv";
+$file = fopen($n_dok, 'w');
+$header = array( "id narudzbe", "datum narudzbe", "podaci o kupcu", "podaci o dostavi", 
+"podaci o placanju", "status narudzbe", "podaci o proizvodima");
+fputcsv($file, $header);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $id_narudzbe = $row['order_id'];
+        $id_proizvoda = $row['product_id'];
+        $podaci_proizvoda = $row['order_item_name'];
+        $datum_narudzbe = $row['order_date'];
+        $status_narudzbe = $row['status'];
+        $kreirano = $row['date_created'];
+        $id_kupac = $row['customer_id'];
+        $cijena = $row['product_gross_revenue'];
+
+        $podaci = array($id_narudzbe, $id_proizvoda, $podaci_proizvoda, $datum_narudzbe, $status_narudzbe, $kreirano, $id_kupac, $cijena);
+        fputcsv($file, $podaci);
+    }
+}
+
+fclose($file);
+
+// Download the CSV file
+header('Content-Type: application/csv');
+header('Content-Disposition: attachment; filename="' . $n_dok . '";');
+
+readfile($n_dok);
+
+// Close database connection
+$conn->close();
+?>
